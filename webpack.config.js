d1d4324be5b579c5b3a764f093e4b58434d35b72
Kevin O'Rourke <orourke.kevin@gmail.com>
2020-11-02 14:13:40 -0500

    Add webpack + dev server + basic config

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const root = path.resolve(__dirname, '../');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'eval-source-map',
+  devServer: {
+    open: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg|xml)$/i,
+        use: 'file-loader'
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin({ root }),
+    new webpack.DefinePlugin({
+      CANVAS_RENDERER: JSON.stringify(true),
+      WEBGL_RENDERER: JSON.stringify(true)
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
+};
