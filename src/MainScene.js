390ba60b44878b21986fd135203086e957788050
Kevin O'Rourke <orourke.kevin@gmail.com>
2020-11-04 12:08:51 -0500

    Add rocket asset

--- src/MainScene.js
+++ src/MainScene.js
@@ -1,5 +1,5 @@
 import { Scene } from 'phaser';
-import logoImg from './assets/logo.png';
+import shipImg from './assets/rocket_192.png';
 
 export class MainScene extends Scene {
   constructor(){
@@ -7,19 +7,30 @@ export class MainScene extends Scene {
   }
 
   preload() {
-    this.load.image('logo', logoImg);
+    this.load.image('ship', shipImg);
   }
 
   create() {
-    const logo = this.add.image(400, 150, 'logo');
+    this.createShip();
+  }
 
-    this.tweens.add({
-      targets: logo,
-      y: 450,
-      duration: 2000,
-      ease: 'Power2',
-      yoyo: true,
-      loop: -1
-    });
+  createShip() {
+    const gameBounds = this.physics.world.bounds;
+    this.ship = this.add.image(
+      gameBounds.width / 2,
+      gameBounds.height / 2,
+      'ship'
+    );
+
+    this.ship.setScale(0.5);
+
+    const shipHeight = this.ship.displayHeight;
+    const shipWidth = this.ship.displayWidth;
+
+    // place ship at bottom edge of the scene
+    this.ship.setPosition(
+      gameBounds.width / 2,
+      gameBounds.height - (shipHeight / 2),
+    );
   }
 }
