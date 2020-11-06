4418301706df9a9e99e8002d139cbd90ff969275
Kevin O'Rourke <orourke.kevin@gmail.com>
2020-11-05 20:24:03 -0500

    Add sky gradient

--- src/MainScene.js
+++ src/MainScene.js
@@ -49,12 +49,42 @@ export class MainScene extends Scene {
     this.cameras.main.startFollow(this.ship);
     this.cameras.main.followOffset.set(0, 100);
 
+    this.sky = this.createSky();
+
     this.keyboard = this.input.keyboard.addKeys({
       up: Phaser.Input.Keyboard.KeyCodes.W,
       down: Phaser.Input.Keyboard.KeyCodes.S,
       left: Phaser.Input.Keyboard.KeyCodes.A,
       right: Phaser.Input.Keyboard.KeyCodes.D,
     });
+  }
+
+  createSky() {
+    const worldBounds = this.physics.world.bounds;
+    const width = worldBounds.width;
+    const height = (this.cameras.main.height * 1.75);
+    const texture = this.textures.createCanvas(
+      'skyGradient',
+      width,
+      height,
+    );
+    const context = texture.getContext();
+    const grd = context.createLinearGradient(0, 0, 0, height);
+
+    grd.addColorStop(0, 'rgba(125, 225, 255, 0)');
+    grd.addColorStop(1/2, 'rgba(125, 225, 255, 0.9)');
+    grd.addColorStop(1, 'rgba(62, 191, 255, 1)');
+
+    context.fillStyle = grd;
+    context.fillRect(0, 0, width, height);
+
+    texture.refresh();
+
+    this.add.image(
+      worldBounds.left,
+      worldBounds.bottom,
+      'skyGradient'
+    ).setOrigin(0, 1);
   }
 
   update(time, delta) {
