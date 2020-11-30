8b703667307294380e12177b304314849b940e5d
Kevin O'Rourke <orourke.kevin@gmail.com>
2020-11-30 09:56:31 -0500

    Adjust ship physics

--- src/objects/player.ts
+++ src/objects/player.ts
@@ -6,9 +6,9 @@ export class Player extends GameObjects.Container {
 
   // Amount of drag to be applied for each additional object in the container
   public readonly DRAG_FACTOR = 25;
-  public readonly MAX_VELOCITY = 275;
+  public readonly MAX_VELOCITY = 250;
   public readonly ANGULAR_DRAG = 100;
-  public readonly BASE_ANGULAR_ACCEL = 200;
+  public readonly BASE_ANGULAR_ACCEL = 225;
   public readonly BASE_ANGLE = -90;
 
   public attachedAsteroids = {
